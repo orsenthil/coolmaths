651a3f117bcee863a5ae91409f7b6510b72f7fec
Senthil Kumaran <senthil@python.org>
2023-04-07 23:09:49 -0700

    full screen.

--- main.go
+++ main.go
@@ -2,102 +2,50 @@ package main
 
 import (
 	"fmt"
-	"os"
+	"log"
+	"time"
 
 	tea "github.com/charmbracelet/bubbletea"
 )
 
-type model struct {
-	choices  []string         // items on the to-do list
-	cursor   int              // which to-do item our cursor is point at
-	selected map[int]struct{} // which to-do items are selected
-}
+type model int
 
-func initialModel() model {
-	return model{
-		// Our to-do list is a grocery list
-		choices:  []string{"Buy carrots", "Buy Celery", "Buy Kohlrabi"},
-		selected: make(map[int]struct{}),
+type tickMsg time.Time
+
+func main() {
+	p := tea.NewProgram(model(5), tea.WithAltScreen())
+	if _, err := p.Run(); err != nil {
+		log.Fatal(err)
 	}
-}
 
+}
 func (m model) Init() tea.Cmd {
-	// Just return `nil`, which means "no I/O right now, please."
-	return nil
+	return tea.Batch(tick(), tea.EnterAltScreen)
 }
 
-func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
-	switch msg := msg.(type) {
-
-	// Is it a key press?
-
+func (m model) Update(message tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := message.(type) {
 	case tea.KeyMsg:
 		switch msg.String() {
-		case "ctrl+c", "q":
+		case "q", "esc", "ctrl+c":
 			return m, tea.Quit
-
-		// The "up" and "k" m.keys move the cursor up
-		case "up", "k":
-			if m.cursor > 0 {
-				m.cursor--
-			}
-		case "down", "j":
-			if m.cursor < len(m.choices)-1 {
-				m.cursor++
-			}
-
-		// The enter key and the spacebar(a literal space) toggle
-		// the selected state for the item that the cursor is pointing at.
-		case "enter", " ":
-			_, ok := m.selected[m.cursor]
-			if ok {
-				delete(m.selected, m.cursor)
-			} else {
-				m.selected[m.cursor] = struct{}{}
-
-			}
-
 		}
+	case tickMsg:
+		m--
+		if m <= 0 {
+			return m, tea.Quit
+		}
+		return m, tick()
 	}
-
 	return m, nil
 }
 
 func (m model) View() string {
-	// The header
-	s := "What should we buy at the market?\n\n"
-
-	// Iterate over our choices
-	for i, choice := range m.choices {
-		// Is the cursor pointing at this choice
-		cursor := " " // no cursor
-
-		if m.cursor == i {
-			cursor = ">" // cursor !
-		}
-
-		// Is this choice selected ?
-		checked := " " // not selected
-		if _, ok := m.selected[i]; ok {
-			checked = "x" // selected
-		}
-
-		// Render the row
-		s += fmt.Sprintf("%s [%s] %s\n", cursor, checked, choice)
-	}
-
-	// The footer
-	s += "\nPress q to quit.\n"
-
-	// Send the UI for rendering
-	return s
+	return fmt.Sprintf("\n\n Hi. This program will exit in %d seconds...", m)
 }
 
-func main() {
-	p := tea.NewProgram(initialModel())
-
-	if _, err := p.Run(); err != nil {
-		fmt.Printf("Alas, there's been an error: %v", err)
-		os.Exit(1)
-	}
+func tick() tea.Cmd {
+	return tea.Tick(time.Second, func(t time.Time) tea.Msg {
+		return tickMsg(t)
+	})
 }
